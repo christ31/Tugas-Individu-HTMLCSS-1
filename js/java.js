0c4d1e4ee5a5478fbedc5d4752e7ff5fbd8bb53d
christ31 <natanniel31@gmail.com>
2023-03-21 17:03:41 +0700

    index.html: Add Animation on Scroll

---
+++ js/java.js
@@ -0,0 +1,76 @@
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
+
+
+
+/*
+let options = {
+  threshold: 1.0
+}
+// Create the observer
+const observer = new IntersectionObserver(entries => {
+  // We will fill in the callback later...
+
+  // Loop over the entries
+  entries.forEach(entry => {
+    // If the element is visible
+    if (entry.isIntersecting) {
+      // Add the animation class
+      entry.target.classList.add('main-card-container-fly-down');
+    }
+  });
+});
+
+let element = ".main-card-container";
+
+
+
+document.querySelectorAll(element).forEach((i) => {
+  if (i){
+    observer.observe(i);
+  }
+})
+
+*/
+// ! querySelector only return the first list of the class
+// ! querySelectorAll return every 
+
+
